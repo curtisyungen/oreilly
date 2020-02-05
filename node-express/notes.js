4591332dcb19d0f8d645fda1263b049d8cac8fe1
Curtis Yungen <curtisyungen@gmail.com>
2020-02-04 20:54:18 -0800

    update notes

--- node-express/notes.js
+++ node-express/notes.js
@@ -1,6 +1,10 @@
-// Terminal, to learn:
+// TO LEARN
+
+// ***** Terminal *****
 // Search for text in files
 // Search for files and directories
 // Chain commands together
 // Copy, move, and delete files
 // Redirect output
+
+// ***** Regex *****
