acf9f740834310ce90b0007ec5217fc5908fbb85
Curtis Yungen <curtisyungen@gmail.com>
2020-03-07 15:25:07 -0800

    add c-sharp dir

---
+++ c-sharp/c-sharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 100000001;
+            float f = i1;
+            int i2 = (int)f;
+            Console.WriteLine(i2);
+        }
+    }
+}
