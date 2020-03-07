626d8562d409e8884c83845a7233c3db4a10ab24
Curtis Yungen <curtisyungen@gmail.com>
2020-03-07 15:33:05 -0800

    add c-sharp folder

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -6,10 +6,7 @@ namespace c_sharp
     {
         static void Main(string[] args)
         {
-            int i1 = 100000001;
-            float f = i1;
-            int i2 = (int)f;
-            Console.WriteLine(i2);
+            Console.WriteLine("Hello World!");
         }
     }
 }
